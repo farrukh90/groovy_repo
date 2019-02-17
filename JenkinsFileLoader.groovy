db96b6d6f9bf7f0c48d672a9ef624d930e4d0241
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-17 12:19:37 -0600

    Changed to something else

--- JenkinsFileLoader.groovy
+++ JenkinsFileLoader.groovy
@@ -1,14 +1,11 @@
-#!/usr/bin/env groovy
-
-// Jenkins file or pipeline scripts editor in your job
-new MyClass(this).runBuild()
-
-// Class declaration
 class MyClass implements Serializable {
+
     def script
+
     MyClass(def script) {
         this.script=script
     }
+
     def runBuild() {
         script.echo script.currentBuild.toString()
     }
